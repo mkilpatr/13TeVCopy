d34779c9c8aad601f1a957918754ccc67ccbf06f
Nick McColl <nickmccoll@gmail.com>
2014-08-25 12:58:15 +0200

    analyzer_configuration was created to store the nominal configuration of all jobs

---
+++ AnalysisBase/Analyzer/python/analyzer_configuration_cfi.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Config as cms
+
+baseAnalyzer_configuration = cms.PSet(
+                                      outputPath       = cms.untracked.string ('plots.root'), #give an output path if you want plotter to produce one
+                                      outputInterval   = cms.untracked.int32 (-9),            #how rewuesntly to update the outputfile
+                                      randomSeed       = cms.uint32 (1234),                   #random seed for the event
+                                      )
+
+physicsAnalyzer_configuration = cms.PSet(
+                                      outputPath       = cms.untracked.string ('plots.root'), #give an output path if you want plotter to produce one
+                                      outputInterval   = cms.untracked.int32 (-9),            #how rewuesntly to update the outputfile
+                                      randomSeed       = cms.uint32 (1234),                   #random seed for the event
+                                                                                              
+                                      isData           = cms.int32  (0),                      #True if running on data
+                                      globalTag        = cms.string ("?"),                    
+                                      process          = cms.string ("?"),                    #Physics process (QCD, TTBAR..)
+                                      dataset          = cms.string ("?"),                    #Full dataset name
+                                      crossSection     = cms.double (-99),                    #Input process cross section
+                                      totalEvents      = cms.int32  (-99),                    #Total number of ecents in the sample
+                                      crossSectionScaling     = cms.double (1),               #Luminosity to scale to (given the cross section and total events)
+                                      genEventInfoSource      = cms.InputTag (""),                                                                                                                                  
+                                      )
+
+nominal_configuration = cms.PSet(
+                                      outputPath       = cms.untracked.string ('plots.root'), #give an output path if you want plotter to produce one
+                                      outputInterval   = cms.untracked.int32 (-9),            #how rewuesntly to update the outputfile
+                                      randomSeed       = cms.uint32 (1234),                   #random seed for the event
+                                                                                              
+                                      isData           = cms.int32  (0),                      #True if running on data
+                                      globalTag        = cms.string ("?"),                    
+                                      process          = cms.string ("?"),                    #Physics process (QCD, TTBAR..)
+                                      dataset          = cms.string ("?"),                    #Full dataset name
+                                      crossSection     = cms.double (-99),                    #Input process cross section
+                                      totalEvents      = cms.int32  (-99),                    #Total number of ecents in the sample
+                                      crossSectionScaling     = cms.double (1),               #Luminosity to scale to (given the cross section and total events)
+                                      genEventInfoSource      = cms.InputTag (""),                                                                                
+                                      
+                                      vertices          = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                      mets              = cms.InputTag("slimmedMETs"),                                                                                                                                                                                              
+                                      )
