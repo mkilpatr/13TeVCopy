6c1d5fe1bc4b0d64d55112d0a2b645a858d066b1
Nick McColl <nickmccoll@gmail.com>
2014-11-03 16:16:13 +0100

    Added T2bW kinematics calculations (leaving out specific Q/G ones)

--- AnalysisTools/KinematicVariables/interface/JetKinematics.h
+++ AnalysisTools/KinematicVariables/interface/JetKinematics.h
@@ -8,6 +8,7 @@
  * want to place any addition pT or eta requirements
  *
  * Jet pT sorting is assumed!!
+ *
  */
 #include <vector>
 #include <utility>
@@ -18,16 +19,13 @@ namespace ucsbsusy {
 
 
 namespace JetKinematics {
-  const double noDist = 999;
+  const double noDist = 10.7;
 
   template<typename Jet>
   Jet mht(const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999, const std::vector<bool>* selected = 0);
 
   template<typename Jet>
   double ht(const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999, const std::vector<bool>* selected = 0);
-
-//  template<typename Jet>
-//  int numJets(const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999){return PhysicsUtilities::countObjectsDeref(jets,minPT,maxEta);}
 
   template<typename MET,typename Jet>
   double absDPhiMETJ(const MET& met, const Jet& jet, const double minPT = 0, const double maxEta = 9999);
@@ -38,14 +36,34 @@ namespace JetKinematics {
   template<typename MET,typename Jet>
   double absDPhiMETJ3(const MET& met, const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999);
 
+  template<typename Momentum1, typename Momentum2>
+  double transverseMass(const Momentum1& visible, const Momentum2& invisible);
+
+  template<typename MET,typename Jet>
+  double htAlongHtAway(const MET& met, const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999);
+
+  template<typename Jet>
+  double ptRMS(const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999);
+
+  template<typename MET,typename Jet>
+  double deltaPhiMETRMS(const MET& met, const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999);
+
+  template<typename Jet>
+  double bJetInvMass(const std::vector<Jet*>& jets, bool (*isBJet)(const Jet&), const double minPT = 0, const double maxEta = 9999);
+
+  template<typename MET,typename Jet>
+  double bJetTranverseMass(const MET& met, const std::vector<Jet*>& jets, bool (*isBJet)(const Jet&), const double minPT = 0, const double maxEta = 9999);
+
+  template<typename Jet>
+  double deltaEtaBJetRMS(const std::vector<Jet*>& jets, bool (*isBJet)(const Jet&), const double minPT = 0, const double maxEta = 9999);
+
+  template<typename Jet>
+  double highestPTJetPair(const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999);
+
 
 
   template<typename Jet>
-  bool passCuts(const Jet& jet,const double minPT, const double maxEta);
-
-
-  int dummyFunction();
-
+  static bool passCuts(const Jet& jet,const double minPT, const double maxEta);
 }
 
 }
