90705c71b65f2effde755e0205270e1b45587b15
Nick McColl <nickmccoll@gmail.com>
2014-11-05 13:09:48 +0100

    Forgot to add the cc file

--- AnalysisBase/TreeAnalyzer/src/BaseTreeAnalyzer.cc
+++ AnalysisBase/TreeAnalyzer/src/BaseTreeAnalyzer.cc
@@ -13,13 +13,8 @@ using namespace ucsbsusy;
 
 //--------------------------------------------------------------------------------------------------
 BaseTreeAnalyzer::BaseTreeAnalyzer(TString fileName, TString treeName, bool isMCTree, TString readOption) : reader(fileName,treeName,readOption),  isMC(isMCTree)
-    , ak4Jets (0)
-    , ak4GenJets(0)
-    , electrons(0)
-    , muons(0)
-    , taus(0)
 {
-  cout << "Running over: " << (isMC ? "MC" : "data") <<endl;
+  clog << "Running over: " << (isMC ? "MC" : "data") <<endl;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -33,26 +28,21 @@ void BaseTreeAnalyzer::load(VarType type, int options, string branchName)
     case AK4JETS : {
       int defaultOptions = JetReader::defaultOptions | (isMC ? JetReader::LOADGEN : JetReader::NULLOPT);
       reader.load(&ak4Reader, options < 0 ? defaultOptions : options, branchName == "" ? "ak4pfchs" : branchName);
-      ak4Jets = &ak4Reader.recoJets;
-      ak4GenJets = &ak4Reader.genJets;
       break;
     }
     case ELECTRONS : {
       int defaultOptions = ElectronReader::defaultOptions;
       reader.load(&electronReader, options < 0 ? defaultOptions : options, branchName == "" ? "ele" : branchName );
-      electrons = &electronReader.electrons;
       break;
     }
     case MUONS : {
       int defaultOptions = MuonReader::defaultOptions;
       reader.load(&muonReader, options < 0 ? defaultOptions : options, branchName == "" ? "mu" : branchName );
-      muons = &muonReader.muons;
       break;
     }
     case TAUS : {
       int defaultOptions = TauReader::defaultOptions;
       reader.load(&tauReader, options < 0 ? defaultOptions : options, branchName == "" ? "tau" : branchName );
-      taus = &tauReader.taus;
       break;
     }
     default : {
