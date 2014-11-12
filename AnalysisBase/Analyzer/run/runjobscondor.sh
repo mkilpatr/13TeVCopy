bdd3e138bfaae3a52e2dc359bef1d9acfe9e4dfc
vdutta <vdutta@mit.edu>
2014-11-12 22:35:14 +0100

    scripts to help with submitting jobs for ntuple production

---
+++ AnalysisBase/Analyzer/run/runjobscondor.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cfgfile=$1
+filename=$2
+outputdir=$3
+outputname=$4
+maxevents=$5
+scramdir=$6
+
+workdir=`pwd`
+
+echo `hostname`
+echo "${_CONDOR_SCRATCH_DIR}"
+echo "scramdir: $scramdir"
+echo "workdir: $workdir"
+echo "args: $*"
+ls -l
+
+source /uscmst1/prod/sw/cms/shrc prod
+cd $scramdir/src/
+SCRAM_ARCH=slc6_amd64_gcc481
+eval `scramv1 runtime -sh`
+cd $workdir
+
+cp ${scramdir}/${cfgfile} .
+cp ${scramdir}/${filename} .
+
+cmsRun $cfgfile print inputFiles_clear inputFiles_load=${filename} outputFile=${outputname} maxEvents=${maxevents}
+
+status=`echo $?`
+echo "Status = $status"
+
+exit $status
