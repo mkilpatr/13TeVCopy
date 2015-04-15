e036be27c51f328e1c5d1befdb030e49fa74066a
Loukas Gouskos <Loukas.Gouskos@cern.ch>
2015-04-15 22:32:51 +0200

    add class to make pseudo jets

---
+++ AnalysisTools/KinematicVariables/interface/CreatePseudoJets.h
@@ -0,0 +1,34 @@
+#ifndef CREATEPSEUDOJETS_H
+#define CREATEPSEUDOJETS_H
+#include <vector>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
+#include "TLorentzVector.h"
+#include "TFitter.h"
+#include "TString.h"
+#include "AnalysisTools/DataFormats/interface/Momentum.h"
+#include "AnalysisTools/DataFormats/interface/Lepton.h"
+#include "AnalysisTools/DataFormats/interface/Jet.h"
+
+using namespace ucsbsusy;
+
+class CreatePseudoJets {
+ public:
+
+  CreatePseudoJets();
+  ~CreatePseudoJets();
+
+
+  // operations
+  void getInitAxis(const std::vector<RecoJetF*>& jets,RecoJetF &initJet1,RecoJetF &initJet2,unsigned int &indx1,unsigned int &indx2);
+  float getLundDistance(RecoJetF JetAxis,RecoJetF *tmpJet);
+  void makePseudoJets(const std::vector<RecoJetF*>& jets,RecoJetF &pseudoJet1, RecoJetF &pseudoJet2);
+
+}; // end of class CreatePseudoJets
+
+#endif
---
+++ AnalysisTools/KinematicVariables/src/CreatePseudoJets.cc
@@ -0,0 +1,78 @@
+#include "../interface/CreatePseudoJets.h"
+#include "AnalysisTools/Utilities/interface/PhysicsUtilities.h"
+#include "Math/VectorUtil.h"
+
+using namespace std;
+using namespace ucsbsusy;
+
+
+CreatePseudoJets::CreatePseudoJets() {}
+CreatePseudoJets::~CreatePseudoJets(){}
+
+void CreatePseudoJets::getInitAxis(const std::vector<RecoJetF*>& jets,RecoJetF &initJet1,RecoJetF &initJet2,unsigned int &indx1,unsigned int &indx2) {
+
+  if (jets.size()<2) { 
+    initJet1.p4().SetPxPyPzE(0.0001,0.0001,0.0001,0.0001);
+    initJet2.p4().SetPxPyPzE(0.0001,0.0001,0.0001,0.0001); }
+  
+
+  float invMassMax = -1.;
+  for (UInt_t i1=0; i1<jets.size(); ++i1) {
+    for (UInt_t i2=0; i2<jets.size(); ++i2) {
+
+      if (i1>i2) {
+	float tmpMass = (jets[i1]->p4()+jets[i2]->p4()).M();
+	if (tmpMass>invMassMax) { 
+	  invMassMax = tmpMass; 
+	  indx1 = i1; indx2 = i2; 
+	  initJet1.p4().SetPxPyPzE(jets[i1]->p4().Px(),jets[i1]->p4().Py(),jets[i1]->p4().Pz(),jets[i1]->p4().E());
+	  initJet2.p4().SetPxPyPzE(jets[i2]->p4().Px(),jets[i2]->p4().Py(),jets[i2]->p4().Pz(),jets[i2]->p4().E()); }
+      } else { continue; }
+
+
+    } // end of looping over jets-i2
+  } // end of looping over jets-i1
+
+  
+  //  cout << invMassMax << " " << indx1 << " " << indx2 << " " 
+  //       << initJet1.p4().Pt() << " " << initJet2.p4().Pt()
+  //       << "\n";
+  
+} //end of getInitAxis
+
+
+
+float CreatePseudoJets::getLundDistance(RecoJetF JetAxis,RecoJetF *tmpJet) {
+
+
+  float cosTheta = ROOT::Math::VectorUtil::CosTheta(JetAxis.p4(),tmpJet->p4());
+  float tmpDistance = (JetAxis.p4().E()-(tmpJet->p4().P())*(cosTheta))*((JetAxis.p4().E())/(pow((JetAxis.p4().E()+tmpJet->p4().E()),2)));
+
+  return tmpDistance;
+} //end of getLundDistance()
+
+
+void CreatePseudoJets::makePseudoJets(const std::vector<RecoJetF*>& jets, RecoJetF &pseudoJet1, RecoJetF &pseudoJet2) {
+
+  RecoJetF initJet1; RecoJetF initJet2;
+  unsigned int indx1 = 0; unsigned int indx2 = 0;
+  getInitAxis(jets,initJet1,initJet2,indx1,indx2);
+
+  pseudoJet1 = initJet1;
+  pseudoJet2 = initJet2;
+
+  for  (UInt_t i1=0; i1<jets.size(); ++i1) {
+
+    if ((i1==indx1) || (i1==indx2)) { continue; }
+    RecoJetF *tmpJet = jets[i1]; 
+    float tmp1 = getLundDistance(initJet1,tmpJet);
+    float tmp2 = getLundDistance(initJet2,tmpJet);
+
+    if (tmp1<=tmp2) { pseudoJet1.p4() += tmpJet->p4(); }
+    else            { pseudoJet2.p4() += tmpJet->p4(); }
+
+  }
+
+  cout << "the 2 pseudo-Jets: " << pseudoJet1.p4().Pt() << " " << pseudoJet2.p4().Pt() << "\n";
+
+} // end of makePseudoJets
