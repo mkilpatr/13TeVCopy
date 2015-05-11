0a5a6b6a0f0a2a1dfcc6119aeef0fb1e9d085b3b
Loukas Gouskos <Loukas.Gouskos@cern.ch>
2015-05-11 17:06:12 +0200

    fix x-section calculation when multiple files used

--- AnalysisTools/TreeReader/interface/Defaults.h
+++ AnalysisTools/TreeReader/interface/Defaults.h
@@ -17,7 +17,7 @@ namespace defaults {
   const std::string BRANCH_AK4JETS   = "ak4";
   const std::string BRANCH_PICKYJETS = "picky";
   enum Process {DATA, QCD, TTBAR, SINGLE_W, SINGLE_Z, SINGLE_G,SINGLE_T, TTZ, TTW,DIBOSON,SIGNAL, NUMPROCESSES };
-  const std::string PROCESS_NAMES[] = {"data","qcd","ttbar","w","z","g","t","ttZ","ttw","diboson","signal",""};
+  const std::string PROCESS_NAMES[] = {"data","qcd","ttbar","w","z","g","t","ttZ","ttW","diboson","signal",""};
   const std::string BRANCH_PUPPIJETS = "ak4puppi";
   const std::string BRANCH_CA8JETS ="ca8";
   const std::string BRANCH_ELECTRONS = "ele";
