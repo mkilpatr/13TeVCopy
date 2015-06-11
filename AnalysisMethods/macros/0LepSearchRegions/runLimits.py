4730b57e16e153f38e3897cd5712a35e3d4652d8
samm0625 <mullin@umail.ucsb.edu>
2015-06-11 23:48:50 +0200

    setup to run diffrent limits; about to overhaul to use the same config file as makeDatacards.py

--- AnalysisMethods/macros/0LepSearchRegions/runLimits.py
+++ AnalysisMethods/macros/0LepSearchRegions/runLimits.py
@@ -23,18 +23,18 @@
 datacard and then runs the limits. At the moment it assumes that the datacards for each
 signal point are in their own folder and combines all files in each signal point's folder.
 
-It currently runs the "ExpSign" mode from Loukas's code (changing '-t -1' to '-t 500'; see 
-below).
+It currently runs HybridNew, ProfileLikelihood, and Asymptotic.
 """
 
 import os
 import commands
 import sys 
+import time 
 
 # to get the subdirectory
 args = sys.argv[1:] 
 if len(args)<1:
-  print "You need to give me a subdirectory! (the same one from makeDatacards.py; also used for printLimits.py)"
+  print "You need to give me a subdirectory! (the same one from makeDatacards.py)"
   print "$ ./runLimits subdir_from_makeDatacards"
   sys.exit(1)
 subdir = args[0]
@@ -42,27 +42,51 @@ subdir = args[0]
   
 # ===== user defined stuff =====
 
-sigPoints = ['T2tt_850_100','T2tt_650_325','T1tttt_1200_800','T1tttt_1500_100'] 
+sigPoints = ['T2tt_850_100','T2tt_650_325','T1tttt_1500_100','T1tttt_1200_800'] 
+
+# method choices: HybridNew, ProfileLikelihood, Asymptotic
+limitMethod = 'Asymptotic'
 
 # you probably don't need to change this unless you want different directory names
 saveLocation = 'datacards/'+subdir+'/'  # this should be the same as the one in makeDatacards.py
-outputLocation = 'limitRootFiles/'+subdir+'/' # place to move all the output files from running the limits
+outputLocation = 'limitRootFiles/'+subdir+'_'+limitMethod+'/' # place to move all the output files from running the limits
 
 ##### ##### ##### ##### ##### ##### ##### 
 ##### you should not need to touch  ##### 
 ##### anything below this point!!!  ##### 
 ##### ##### ##### ##### ##### ##### ##### 
 
+##### ##### ##### ##### ##### #####
+#####  define some functions  #####
+##### ##### ##### ##### ##### #####
+
+def getLimit(rootFile):
+  rootCommand = 'root -l -q "../getLimitNumbers.C(\\"'+rootFile+'\\")"'
+  output = commands.getoutput(rootCommand)
+  meanAndError = output.split('\n')[-1]
+  return meanAndError
+
+##### ##### ##### #####
+##### actual work #####
+##### ##### ##### #####
+
 if not os.path.exists(saveLocation):
   print saveLocation, 'does not exist!' 
   print 'Are you sure', subdir, 'is the one you used in makeDatacards.py?'
   sys.exit(1)
 
+currentDir = os.getcwd()
+significances = [] # store the Significance from each sig point to print at the end
 
-for sigPoint in sigPoints:
-  currentFiles = os.listdir('./') # needed to move the output files correctly at the end of each sigPoint 
-  
-  datacardSaveLocation = saveLocation + sigPoint+'/'
+for sigPoint in sigPoints: 
+  # create and move into a dummy directory (using the timestamp in the name for uniqueness). At the end of 
+  # each sigPoint, all remaining files will be moved to an actual storage location 
+  dummyRunDir = 'dummy_' + str(int(time.time()*1e8))
+  print 'creating a dummy directory to run in:', dummyRunDir 
+  os.makedirs(dummyRunDir)
+  os.chdir(dummyRunDir)
+    
+  datacardSaveLocation = os.path.join(currentDir,saveLocation + sigPoint+'/')
   datacards = os.listdir(datacardSaveLocation)
   combinedDatacard = 'combined_'+sigPoint+'.txt'
   
@@ -76,33 +100,112 @@ for sigPoint in sigPoints:
   output = commands.getoutput(combineDatacardsCommand)
   print output # probably keep this in case of datacard syntax problems
   
-  #=== run the limits ===
-  # this currently uses "ExpSign" from Loukas's code 
-  
-  limitsOutputFile = 'limitsOutput_' + sigPoint 
-  runLimitsCommand =  'combine -M ProfileLikelihood '+combinedDatacard+' --significance -t 500 --expectSignal=1 -n '+limitsOutputFile
+  # ===== run the limits ===== 
+  #
+  ### notes from previous versions:
+  # runLimitsCommand =  'combine -M ProfileLikelihood '+combinedDatacard+' --significance -t 500 --expectSignal=1 -n '+limitsOutputFile
   # for now, remove '--pvalue' (between --significance and -t 500)
   # note: the default for -t is -1 (Asimov dataset) see https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideHiggsAnalysisCombinedLimit
-  print 'now running:' #DEUBGGING ONLY 
-  print runLimitsCommand, '\n' # keep this in some kind of log file?
-  output = commands.getoutput(runLimitsCommand)
-  print output # maybe redirect this to some kind of log file?
   
+  # === Asymptotic
+  if limitMethod=='Asymptotic':
+    runLimitsCommand =  'combine -M ProfileLikelihood '+combinedDatacard+' --significance -t -1 --expectSignal=1 -n '+sigPoint
+    # run the limit command and figure out what the output root file is
+    print 'now running:' #DEUBGGING ONLY 
+    print runLimitsCommand, '\n' # keep this in some kind of log file?
+    output = commands.getoutput(runLimitsCommand)
+    print output # maybe redirect this to some kind of log file?
+    
+    # pull the Significance out and store
+    tempSig = ''
+    for line in output.split('\n'): 
+      if 'Significance' in line: 
+        tempSig = line.replace('Significance',sigPoint) 
+    if tempSig=='': 
+      significances.append(sigPoint+': no significance found..')
+    else:
+      significances.append(tempSig)  
+  #limitMethod=='Asymptotic'
+  
+  # === ProfileLikelihood
+  elif limitMethod=='ProfileLikelihood':
+    runLimitsCommand =  'combine -M ProfileLikelihood '+combinedDatacard+' --significance -t 500 --expectSignal=1 -n '+sigPoint
+    # run the limit command and figure out what the output root file is
+    print 'now running:' #DEUBGGING ONLY 
+    print runLimitsCommand, '\n' # keep this in some kind of log file?
+    output = commands.getoutput(runLimitsCommand)
+    print output # maybe redirect this to some kind of log file?
+    
+    # get the significance and store
+    rootFile = ''
+    dummyFiles = os.listdir('./')
+    for df in dummyFiles:
+      if 'higgsCombine' in df: rootFile = df
+    if rootFile=='':
+      significances.append(sigPoint+': no significance found..')
+    else:
+      significances.append( sigPoint + ': ' + getLimit(rootFile) )
+  # limitMethod=='ProfileLikelihood'
+  
+  # === HybridNew method
+  elif limitMethod=='HybridNew':
+    # run the first limit command and figure out what the output root file is
+    runLimitsCommand =  'combine -M HybridNew --frequentist '+combinedDatacard+' --significance --saveToys --fullBToys --saveHybridResult -T 500 -i 10 -n '+sigPoint
+    print 'now running:' #DEUBGGING ONLY 
+    print runLimitsCommand, '\n' # keep this in some kind of log file?
+    output = commands.getoutput(runLimitsCommand)
+    print output # maybe redirect this to some kind of log file?
+    # now need to figure out what the root output of the previous file is
+    dummyFiles = os.listdir('./')
+    outputRootFile = ''
+    for f in dummyFiles:
+      if 'higgsCombine' in f:
+        outputRootFile = f
+    # now run the second limit command with the above root file 
+    runLimitsCommand =  'combine -M HybridNew --frequentist '+combinedDatacard+' --significance --readHybridResult --toysFile='+outputRootFile+' --expectedFromGrid=0.5 -n '+sigPoint
+    print 'now running:' #DEUBGGING ONLY 
+    print runLimitsCommand, '\n' # keep this in some kind of log file?
+    output = commands.getoutput(runLimitsCommand)
+    print output # maybe also redirect this to some kind of log file?
+    
+    # pull the Significance out and store
+    tempSig = ''
+    for line in output.split('\n'): 
+      if 'Significance' in line: 
+        tempSig = line.replace('Significance',sigPoint) 
+    if tempSig=='': 
+      significances.append(sigPoint+': no significance found..')
+    else:
+      significances.append(tempSig)  
+  #limitMethod=='HybridNew':
+  
+  # === non-existant choice of limit method
+  else:
+    print limitMethod, 'is not one of the currently implemented methods! You need to pick one of the allowed ones!'
+    sys.exit(1)
+  #non-existant limit method
+    
   # move any output files to the correct directory
-  allFiles = os.listdir('./')
+  os.chdir(currentDir)
+  dummyFiles = os.listdir(dummyRunDir)
   if not os.path.exists(outputLocation): os.makedirs(outputLocation)
   print '\n'
   print 'moving files to:', outputLocation
-  for f in allFiles:
-    if f not in currentFiles:
-      print ' '*3, f
-      os.rename(f,os.path.join(outputLocation,f)) 
-  # for f in allFiles
-  
+  for f in dummyFiles:
+    print ' '*3, f
+    if 'roostats' in f:
+      os.remove(os.path.join(dummyRunDir,f))
+      continue
+    os.rename(os.path.join(dummyRunDir,f),os.path.join(outputLocation,f)) 
+  #for f in dummyFiles
+  os.removedirs(dummyRunDir)
   print '\n', '='*60, '\n'
   
+#for sigPoint in sigPoints
 
-# for sigPoint in sigPoints
-
+# print the significances
+print '='*5, 'SIGNIFICANCES', '('+limitMethod+')', '='*5
+print '\n'.join(significances)
+print '\n'
 
 
