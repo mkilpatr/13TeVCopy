9540ff25605e14633c91a96a8f49bec3691fb65b
ocolegro <ocolegro@u.rochester.edu>
2015-07-11 11:36:38 +0200

    Update and rename Ratio.C to RatioTCR.C
    Updated Ratio to RatioTCR

--- AnalysisMethods/macros/RatioTCR.C
+++ AnalysisMethods/macros/RatioTCR.C
@@ -24,12 +24,12 @@
    TString  tTree2 = "events";
 ////Rename to your scale factors
    TString scaleName1 = "ScaleFactor";
-   TString scaleName2 = "ScaleFactor";
+   TString scaleName2 = "scaleFactor";
    TString Lumi = "4";
 
 
    void fillHistograms(TString bins, TString lowVal, TString highVal,TString  file1,TString  file2,TString var1,TString var2, TString cut1, TString cut2);
-   bool Ratio(
+   bool RatioTCR(
 		   	   TString outFile = "run/0L/plots/ratioPlot.png",
 		   	   TString cut1 =  "ptMet>200&&nj60>=2&&nJ20>=5&&nmBtag>=1&&mtB01MET>175&&dPhiMET3 > .5 &&  dPhiMET12 > 1 && nmBtag ==1  && NCTTstd == 0",
 		   	   TString cut2 = "ptMet>200&&nj60>=2&&nJ20>=5&&nmBtag>=1&&mtB01MET>175&&dPhiMET3 > .5 &&  dPhiMET12 > 1 && nmBtag > 1  && NCTTstd == 0",
@@ -42,11 +42,11 @@
  			   TString  lowVal = "200.0",
   			   TString highVal = "800.0",
   			   TString var1 = "MET",
-  			   TString var2 = "MET",
+  			   TString var2 = "ptMet",
 			   Float_t  bin = 6
    );
 
-   bool Ratio(TString outFile,TString cut1,TString cut2,TString  file1,TString  file2,TString title,TString selection1, TString selection2,
+   bool RatioTCR(TString outFile,TString cut1,TString cut2,TString  file1,TString  file2,TString title,TString selection1, TString selection2,
 		   TString bins,TString  lowVal,TString highVal,
 		   TString var1,TString var2,Float_t  bin)
    {
@@ -69,7 +69,7 @@
 	   h2->GetXaxis()->SetTitle("");
 	   h2->GetXaxis()->SetLabelOffset(0.20);
 	   h2->GetYaxis()->SetTitle("");
-	   h2->GetYaxis()->SetRangeUser(0,1.1*ymax);
+	   h1->GetYaxis()->SetRangeUser(0,1.1*ymax);
 			 //h1->SetLineColorAlpha(kRed, 0.99);
 			// h2->SetLineColorAlpha(kBlue, 0.99);
 	   h1->SetLineColor(kRed);
@@ -108,7 +108,7 @@
 
 
 	  ////	Format Legend
-	  TLegend *legend=new TLegend(0.6,0.65,0.88,0.85);
+	  TLegend *legend=new TLegend(0.6,0.65,0.93,0.85);
 	  legend->SetTextFont(72);
 	  legend->SetTextSize(0.04);
 	  legend->AddEntry(h1,selection1,"l");
@@ -119,7 +119,7 @@
 
    //// Clone h2 into h3
 	  TH1F* h3 = (TH1F*)h2->Clone("h3"); h3->Divide(h1);	  ymax = h1->GetMaximum()/h2->GetMaximum(); //h3->Divide(h2,h1,1.,1.);
-	  h3->SetTitle(selection1+"/"+selection2+";;");
+	  h3->SetTitle(selection2+"/"+selection1+";;");
 	  if(ymax < h2->GetMaximum()/h1->GetMaximum()) ymax = h2->GetMaximum()/h1->GetMaximum();
 
   //// Building Second pad
@@ -186,9 +186,9 @@
 	  // TH1F* h1Temp = (TH1F*)gPad->GetListOfPrimitives()->FindObject("hist1");
 	   t2->Draw(var2+">>h2",scaleName2+"*"+Lumi+"*("+cut2+")");
 	  // TH1F* h2Temp = (TH1F*)gPad->GetListOfPrimitives()->FindObject("hist2");
-	   h1->Scale(1/h1->Integral(0,(6+1)));
+	  // h1->Scale(1/h1->Integral(0,(6+1)));
 
-	   h2->Scale(1/h2->Integral(0,(6+1)));
+	  // h2->Scale(1/h2->Integral(0,(6+1)));
 	   std::cout << "Getting hist entries" << std::endl;
 	   std::cout << "Number of entries in hist 1 is " << h1->GetEntries() << std::endl;
 	   std::cout << "Number of entries in hist 2 is " << h2->GetEntries() << std::endl;
