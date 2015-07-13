6fc75f6869fead59228f213ae362a9c4accee1da
ocolegro <ocolegro@u.rochester.edu>
2015-07-13 15:11:43 +0200

    Create makeZeroLeptonOneLepCRTrees.C

---
+++ AnalysisMethods/macros/makeZeroLeptonOneLepCRTrees.C
@@ -0,0 +1,62 @@
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include "AnalysisMethods/macros/0LepSearchRegions/ZeroLeptonTreeHelper.hh"
+#endif
+
+using namespace ucsbsusy;
+
+class OneLepCRAnalyzer : public ZeroLeptonAnalyzer {
+
+  public :
+
+    OneLepCRAnalyzer(TString fileName, TString treeName, TString outfileName, bool isMCTree,cfgSet::ConfigSet *pars) :
+      ZeroLeptonAnalyzer(fileName, treeName, outfileName, isMCTree, pars) {}
+
+    bool fillEvent() {
+      if(met->pt() < metcut_) return false;
+      if(!goodvertex) return false;
+      if(nSelLeptons<1)      return false;
+
+      TRandom3 rnd(0);
+      float maxLep = nSelLeptons;
+      int whichLep = rnd.Uniform(0.,nSelLeptons);
+      MomentumF* lep = new MomentumF(selectedLeptons.at(whichLep)->p4());
+
+      if(fabs(PhysicsUtilities::deltaPhi(*met, *lep)) > 1)        return false;
+
+      filler.fillEventInfo(&data, this);
+      filler.fillJetInfo(&data, jets, bJets, met);
+      return true;
+    }
+
+};
+
+void makeZeroLeptonOneLepCRTrees(TString sname = "ttbar_onelepcr",
+                                 const int fileindex = 0,
+                                 const bool isMC = true,
+                                 const TString fname = "/store/user/vdutta/13TeV/080615/merged/ttbar_1_ntuple_wgtxsec.root",
+                                 const double xsec = 1.0,
+                                 const TString outputdir = "trees",
+                                 const TString fileprefix = "root://eoscms//eos/cms")
+{
+
+  printf("Processing file %d of %s sample\n", (fileindex > -1 ? fileindex : 0), sname.Data());
+
+  if(fileindex > -1)
+    sname += TString::Format("_%d",fileindex);
+
+  if(isMC)
+    printf("Cross section: %5.2f pb\n", xsec);
+
+  TString fullname = fileprefix+fname;
+
+  gSystem->mkdir(outputdir,true);
+  TString outfilename = outputdir+"/"+sname+"_tree.root";
+
+  cfgSet::loadDefaultConfigurations();
+  cfgSet::ConfigSet cfg = cfgSet::zl_lepton_set;
+
+  OneLepCRAnalyzer a(fullname, "Events", outfilename, isMC, &cfg);
+
+  a.analyze(100000);
+
+}
