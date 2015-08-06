99c73618b403b7f335f3281a83dcc21ce6f13c85
rdorst <rdorst@umail.ucsb.edu>
2015-08-06 16:46:44 +0200

    MET->HTMHT

--- AnalysisMethods/macros/0LepSearchRegions/makeZeroLeptonOneLepCRTrees.C
+++ AnalysisMethods/macros/0LepSearchRegions/makeZeroLeptonOneLepCRTrees.C
@@ -61,7 +61,7 @@ void makeZeroLeptonOneLepCRTrees(TString sname = "ttbar_onelepcr",
 
   double randSeed = fileindex + 2;
   TString treename = isMC ? "Events" : "TestAnalyzer/Events";
-  DataType type = isMC ? MC : (fname.Contains("met") ? HTMHT : (fname.Contains("singlemu") ? SINGLEMU : (fname.Contains("singleel") ? SINGLEEL : MC)));
+  DataType type = isMC ? MC : (fname.Contains("htmht") ? HTMHT : (fname.Contains("singlemu") ? SINGLEMU : (fname.Contains("singleel") ? SINGLEEL : MC)));
   OneLepCRAnalyzer a(fullname, "Events", outfilename, isMC, &cfg, type, randSeed);
 
   a.analyze(10000);
