6dc419e37fcfabde7e81b3028528416ee24f0f13
ASPatterson <patterson@umail.ucsb.edu>
2015-09-08 09:22:12 +0200

    new CSV WP

--- AnalysisTools/TreeReader/interface/Defaults.h
+++ AnalysisTools/TreeReader/interface/Defaults.h
@@ -39,12 +39,15 @@ namespace defaults {
   const std::string BRANCH_TRIGGERS   = "trig";
   const std::string BRANCH_METFILTERS = "metfilter";
 
-  const double CSV_LOOSE      = 0.423;
-  const double CSV_MEDIUM     = 0.814;
-  const double CSV_TIGHT      = 0.941;
-  const double CSV_OLD_LOOSE  = 0.244;
-  const double CSV_OLD_MEDIUM = 0.679;
-  const double CSV_OLD_TIGHT  = 0.898;
+  const double CSV_LOOSE        = 0.605;
+  const double CSV_MEDIUM       = 0.890;
+  const double CSV_TIGHT        = 0.970;
+  const double CSV_OLD_LOOSE    = 0.423;
+  const double CSV_OLD_MEDIUM   = 0.814;
+  const double CSV_OLD_TIGHT    = 0.941;
+  const double CSV_OLDER_LOOSE  = 0.244;
+  const double CSV_OLDER_MEDIUM = 0.679;
+  const double CSV_OLDER_TIGHT  = 0.898;
   const double TAU_MVA_VETO_MTPRESEL_LOOSE = 0.65;
   const double TAU_MVA_VETO_MTPRESEL_MEDIUM = 0.56;
   const double TAU_MVA_VETO_MTPRESEL_TIGHT = 0.45;
