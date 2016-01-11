2f69de683eed7bf1674827c5c6779c5f17f6563a
Sam <samm0626@gmail.com>
2016-01-11 12:01:55 +0100

    updated default lepton defs to match MT2 and offical SF stuff

--- AnalysisTools/ObjectSelection/src/LeptonId.cc
+++ AnalysisTools/ObjectSelection/src/LeptonId.cc
@@ -49,32 +49,32 @@ namespace LeptonSelection {
   Electron zl_ctr_sLep_electrons (
                               30,
                               2.1,
-                              0.02,
-                              0.1,
+                              -1,
+                              -1,
                               &ElectronID ::mediumNoIso,
                               &ElectronISO::medMini,
                               ZL_CTR_ELE);
   Electron zl_ctr_sLep_sec_electrons (
                                5,
                              2.4,
-                             0.02,
-                             0.1,
+                             -1,
+                             -1,
                              &ElectronID ::mediumNoIso,
                              &ElectronISO::medMini,
                              ZL_CTR_ELE);
   Electron zl_ctr_diLep_electrons(
                               20,
                               2.4,
-                              0.02,
-                              0.1,
+                              -1,
+                              -1,
                               &ElectronID ::mediumNoIso,
                               &ElectronISO::medMini,
                               ZL_CTR_ELE);
   Electron zl_ctr_diLep_sec_electrons(
                               15,
                               2.4,
-                              0.02,
-                              0.1,
+                              -1,
+                              -1,
                               &ElectronID ::mediumNoIso,
                               &ElectronISO::medMini,
                               ZL_CTR_ELE);
@@ -85,7 +85,7 @@ namespace LeptonSelection {
 Muon zl_sel_muons(
                              5,
                             2.4,
-                            0.1,
+                            .2,
                             0.5,
                             &MuonID ::loose,
                             &MuonISO::looseMini,
@@ -93,36 +93,36 @@ Muon zl_sel_muons(
 Muon zl_ctr_sLep_muons(
                              30,
                             2.4,
-                            0.02,
+                            .05,
                             0.1,
                             &MuonID ::medium,
-                            &MuonISO::medMini,
+                            &MuonISO::looseMini,
                             ZL_CTR_MU);
 Muon zl_ctr_sLep_sec_muons(
                              5,
                             2.4,
-                            0.02,
+                            .05,
                             0.1,
                             &MuonID ::medium,
-                            &MuonISO::medMini,
+                            &MuonISO::looseMini,
                             ZL_CTR_MU);
 
 
 Muon zl_ctr_diLep_muons(
                              20,
                             2.4,
-                            0.02,
+                            .05,
                             0.1,
                             &MuonID ::medium,
-                            &MuonISO::medMini,
+                            &MuonISO::looseMini,
                             ZL_CTR_MU);
 Muon zl_ctr_diLep_sec_muons(
                              10,
                             2.4,
-                            0.02,
+                            .05,
                             0.1,
                             &MuonID ::medium,
-                            &MuonISO::medMini,
+                            &MuonISO::looseMini,
                             ZL_CTR_MU);
 }
 
