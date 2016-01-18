73bf7e9ad13dbd4863974d8eff9f3917fbeb67a2
Loukas Gouskos <loukas@cmslpc24.fnal.gov>
2016-01-18 13:47:01 -0600

    i did some changed that i do not remember but added them to merge

--- AnalysisMethods/macros/0LepSearchRegions/makeZeroLeptonSRTrees.C
+++ AnalysisMethods/macros/0LepSearchRegions/makeZeroLeptonSRTrees.C
@@ -23,6 +23,7 @@ void makeZeroLeptonSRTrees(TString sname = "htmht",
   gSystem->mkdir(outputdir,true);
   TString outfilename = outputdir+"/"+sname+"_tree.root";
   cfgSet::ConfigSet pars = pars0lep(json);
+  pars.ttbarCorrections |= ucsbsusy::TtbarCorrectionSet::TOPPAIRPT;
 
   TString treeName = "Events";
   ZeroLeptonAnalyzer a(fullname, treeName, outfilename, fileindex+2, isMC, &pars);
