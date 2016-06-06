5675208a5477030d920c804cfd9816a869f704e8
Loukas Gouskos <loukas@cmslpc36.fnal.gov>
2016-06-06 14:24:17 -0500

    add new mva tau veto and use only 3 jets for min mass in ctt

--- AnalysisBase/TreeAnalyzer/src/DefaultConfigurations.cc
+++ AnalysisBase/TreeAnalyzer/src/DefaultConfigurations.cc
@@ -75,8 +75,8 @@ void cfgSet::loadDefaultTauConfigurations() {
   zl_veto_taus.minPt      = 20;
   zl_veto_taus.maxEta     = 2.4;
   zl_veto_taus.requireOppositeQToSelLepton = false;
-  zl_veto_taus.minDeltaRFromSelLepton      = 0.4;
-  zl_veto_taus.selected   = &ucsbsusy::TauF::ishpsvetotau;
+  zl_veto_taus.minDeltaRFromSelLepton      = -0.4;
+  zl_veto_taus.selected   = &ucsbsusy::TauF::ispogvlisomvarun2new;
   zl_veto_taus.setConfig();
 
   sl_veto_taus = zl_veto_taus;
