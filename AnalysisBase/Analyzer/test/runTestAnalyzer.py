d161c07fb6fe4e8c7efe71c0d1a13bf84572566e
vdutta <vdutta@mit.edu>
2016-07-01 05:15:26 -0500

    revert json change

--- AnalysisBase/Analyzer/test/runTestAnalyzer.py
+++ AnalysisBase/Analyzer/test/runTestAnalyzer.py
@@ -115,7 +115,7 @@ if '/store/data' in DatasetName or re.match(r'^/[a-zA-Z]+/Run[0-9]{4}[A-Z]', Dat
     JECUNCFILE = 'data/JEC/Spring16_25nsV3_DATA_Uncertainty_AK4PFchs.txt'
     import FWCore.PythonUtilities.LumiList as LumiList
     import os
-    jsonFile = os.path.expandvars("$CMSSW_BASE/src/data/JSON/json_DCSONLY_275067_275376_21Jun2016.txt")
+    jsonFile = os.path.expandvars("$CMSSW_BASE/src/data/JSON/json_DCSONLY_275376_21Jun2016.txt")
     # jsonFile = os.path.expandvars("tmp.json")
     process.source.lumisToProcess = LumiList.LumiList(filename=jsonFile).getVLuminosityBlockRange()
     process.TestAnalyzer.isData = cms.int32(1)
