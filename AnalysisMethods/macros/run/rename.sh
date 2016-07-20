aa71aad8cf7e5e9ed1cad50c95b55cfc44d05b2f
Huilin Qu <huilin.qu@cern.ch>
2016-07-20 15:45:50 +0200

    Update EstTools

--- AnalysisMethods/macros/run/rename.sh
+++ AnalysisMethods/macros/run/rename.sh
@@ -18,11 +18,13 @@ cd ..
 cd lepcr/
 mkdir -p $targetdir/onelepcr
 hadd $targetdir/onelepcr/ttbarplusw_onelepcr_tree.root ttbar-mg_tree.root wjets-ht_tree.root tW_tree.root ttW_tree.root
+hadd $targetdir/onelepcr/data_onelepcr_tree.root singleel_tree.root singlemu_tree.root met_tree.root jetht_tree.root doubleeg_tree.root
 cd ..
 
 # photoncr
 cd photoncr
 hadd $targetdir/gjets_photoncr_tree.root gjets_tree.root qcd-fake_tree.root qcd-frag_tree.root ttg_tree.root
+hadd $targetdir/data_photoncr_tree.root singlephoton_tree.root jetht_tree.root doubleeg_tree.root
 cd ..
 
 # link trees
@@ -40,11 +42,11 @@ ln -s ttbarplusw_tree.root ttbarplusw_tradlepcr_tree.root
 ln -s $workdir/sr/met_tree.root data_qcdcr_tree.root
 ln -s $workdir/sr/qcd_tree.root qcd_qcdcr_tree.root
 # photoncr
-ln -s $workdir/photoncr/singlepho_tree.root  data_photoncr_tree.root
+#ln -s $workdir/photoncr/singlepho_tree.root  data_photoncr_tree.root
 
 # onelepcr: for MET+Lep method
 cd $targetdir/onelepcr
-ln -s $workdir/lepcr/singlelep_tree.root data_onelepcr_tree.root
+#ln -s $workdir/lepcr/singlelep_tree.root data_onelepcr_tree.root
 ln -s ../ttbarplusw_tree.root
 ln -s $workdir/onelepcr/signals/*.root ./
 
