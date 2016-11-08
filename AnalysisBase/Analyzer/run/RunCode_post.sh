0b1249537a37eabb66e682b1790ee14a6bc049b9
Matthew <mkilpatr@FNAL.GOV>
2016-11-08 03:48:57 -0600

    Remove shell scripts from repository

--- AnalysisBase/Analyzer/run/RunCode_post.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-for i in "datasets_QCD_post4.conf" "datasets_QCD_post5.conf";
-do
-	./makejobs.py --postprocess -i $i --inputdir /eos/uscms/store/user/mkilpatr/13TeV/ntuples/outputs_crab_merged/ -o /eos/uscms/store/user/mkilpatr/13TeV/230916/merged/
-	source submit_addweight.sh
-done
