02c4c5cdad3999dead7c7656408fb14960553440
Matthew <mkilpatr@FNAL.GOV>
2016-11-08 03:44:53 -0600

    fixed multiple declarations

--- AnalysisTools/KinematicVariables/interface/JetKinematics.h
+++ AnalysisTools/KinematicVariables/interface/JetKinematics.h
@@ -37,9 +37,6 @@ namespace JetKinematics {
   double absDPhiMETJ3(const MET& met, const std::vector<Jet*>& jets, const double minPT = 0, const double maxEta = 9999);
 
   template<typename MET,typename Jet>
-  double absDPhiMETJ(const MET& met, const Jet& jet, const double minPT = 0, const double maxEta = 9999);
-
-  template<typename MET,typename Jet>
   double absDPhiStarMETJ(const MET& met, const Jet& jet, const double minPT = 0, const double maxEta = 9999);
 
   template<typename MET,typename Jet>
